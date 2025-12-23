af39cc8e3f33ff749ed36ecfee652ea55ef7ad74
chechu776 <shameerchechu@gmail.com>
2025-12-23 15:47:48 +0530

    mobile updated card

--- components/sections/BestSellers.jsx
+++ components/sections/BestSellers.jsx
@@ -60,7 +60,7 @@ export default function BestSellers() {
                                     className="w-full h-full object-cover transition-transform duration-700 group-hover:scale-110"
                                 />
 
-                                <div className="absolute bottom-0 left-0 right-0 p-4 translate-y-full group-hover:translate-y-0 transition-transform duration-300">
+                                <div className="absolute bottom-0 left-0 right-0 p-4 translate-y-0 md:translate-y-full md:group-hover:translate-y-0 transition-transform duration-300">
                                     <a
                                         href={`https://wa.me/919995618543?text=I am interested in ${product.name}`}
                                         target="_blank"
